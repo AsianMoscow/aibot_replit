a243e96a06a377bd59a20dd86dccc986abe37b61
Asian <theavsv@gmail.com>
2024-04-23 07:32:51 +0300

    aiogram болванка

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
-print('hello im ai bot')
-print("Измениле текст в pycharm, test")
+import asyncio
+import logging
+import sys
+from os import getenv
+from datetime import datetime
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = getenv("BOT_TOKEN")
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        await message.reply("Я просто болванка! " + str(datetime.now()))
+    except TypeError:
+        with open('errors.log', 'a') as f:
+            print(f"Ошибка! {str(datetime.now())}")
+            f.write(f"Ошибка! {str(datetime.now())}")
+
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
